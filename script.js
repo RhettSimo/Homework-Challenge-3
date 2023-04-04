b3288ca8da73e76aab48716c116fc53ab77cbc4c
RhettSimo <simonsonrhett1337@gmail.com>
2023-04-04 15:59:59 -0500

    Added working code to the assignment.

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  let passwordLength = parseInt(prompt("How long do you want your password to be?"));
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Error. Please choose a password more than 8 characters and less than 128 characters.");
+    return "";
+  }
+
+
+var includeLowercase = confirm("Include lowercase letters in your password?");
+var includeUpperCase = confirm("Include uppercase letters in your password?");
+var includeNumbers = confirm("Include numbers in your password?");
+var includeSpecialCharacters = confirm("Include special characters in your password?");
+
+if (!includeLowercase && !includeSpecialCharacters && !includeUpperCase && !includeNumbers) {
+  alert("Error. Please choose at least one character type.");
+  return "";
+}
+let passwordCharacters = [];
+const specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "1234567890"
+
+if (includeSpecialCharacters) {
+  passwordCharacters = passwordCharacters.concat(specialCharacters.split(""));
+}
+
+if (includeLowercase) {
+  passwordCharacters = passwordCharacters.concat(lowercase.split(""));
+}
+
+if (includeUpperCase) {
+  passwordCharacters = passwordCharacters.concat(upperCase.split(""));
+}
+
+if (includeNumbers) {
+  passwordCharacters = passwordCharacters.concat(numbers.split(""));
+}
+let results = ""; {
+for (var i = 0; i < passwordLength; i++) {
+  let randomIndex = Math.floor(Math.random() * passwordCharacters.length);
+  let randomCharacter = passwordCharacters[randomIndex];
+  results += randomCharacter;
+  }
+}
+
+return results;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
